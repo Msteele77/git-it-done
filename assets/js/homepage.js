a6e87826d8343c24f67549660b2ec785cd6a519a
Macheya Steele <mcheysteele@gmail.com>
2020-12-08 19:04:45 -0700

    updated request logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,15 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
-  
-  getUserRepos();
+
+//Function to request and JSON repos
+var getUserRepos = function(user) {
+
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+getUserRepos();
