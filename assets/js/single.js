f7544746bd4ab611cafc47bf0d48b22189c04161
Macheya Steele <mcheysteele@gmail.com>
2020-12-09 12:08:36 -0700

    addressed possible issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
     console.log(repo);
@@ -14,6 +14,11 @@ var getRepoIssues = function (repo) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                displayWarning(repo);
+                }
             });
         }
         else {
@@ -58,6 +63,22 @@ var displayIssues = function(issues) {
 issueEl.appendChild(typeEl);
     }
     };
+
+
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    //will direct user to go to github if more than 30 issues
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 
 getRepoIssues("facebook/react");
